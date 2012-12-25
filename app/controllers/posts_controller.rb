496a2af980386df6ebb6021532bcb3db7570bd8f
arkiver <akshay@reversehack.in>
2012-12-25 15:57:28 +0530

    minor refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all(:order => [:updated_at.desc], :deleted => 0)
+    @posts = Post.all(:order => [:updated_at.desc], :deleted => false)
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
+
+
 class Post
   include DataMapper::Resource
 
